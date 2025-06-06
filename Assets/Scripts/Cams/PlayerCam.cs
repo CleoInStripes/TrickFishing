70786d1b8b1b62fa0cf00cc5c53fd5bb87f4a427
Azee <aziztitu1996@gmail.com>
2025-06-06 14:04:41 -0700

    Fixes issue from merge

--- Assets/Scripts/Cams/PlayerCam.cs
+++ Assets/Scripts/Cams/PlayerCam.cs
@@ -4,27 +4,12 @@ public class PlayerCam : SingletonMonoBehaviour<PlayerCam>
 {
     public Transform cameraPosition;
 
-    public float mouseSenseX = 80f;
-    public float mouseSenseY;
-
-    float xRotation = 0f;
-    float yRotation = 0f;
-
-    public Transform playerOrientation;
-
     public Camera cam;
 
     private new void Awake()
     {
         base.Awake();
         cam = GetComponentInChildren<Camera>();
-    }
-
-    void Start()
-    {
-        //lock cursor to center of screen
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
     }
 
     void Update()

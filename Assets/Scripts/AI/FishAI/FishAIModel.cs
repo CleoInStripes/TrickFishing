9354a3eb4b745b59923b893ac63f914d25b9bcbf
Azee <aziztitu1996@gmail.com>
2025-06-06 02:24:15 -0700

    Updates Fish AI

--- Assets/Scripts/AI/FishAI/FishAIModel.cs
+++ Assets/Scripts/AI/FishAI/FishAIModel.cs
@@ -1,17 +1,38 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class FishAIModel : MonoBehaviour
 {
+    public Transform avatarRoot;
+    public RangeFloat avatarYOffsetRange;
+
+    [Header("Swimming")]
+    public RangeFloat swimYRange;
+    public RangeFloat swimYDurationRange;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        RandomizeYOffset();
+        PlaySwimmingAnimationAlongYAxis();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void RandomizeYOffset()
+    {
+        avatarRoot.localPosition += Vector3.up * avatarYOffsetRange.GetRandom();
+    }
+
+    void PlaySwimmingAnimationAlongYAxis()
+    {
+        avatarRoot.DOMoveY(avatarRoot.position.y + swimYRange.GetRandom(), swimYDurationRange.GetRandom())
+                 .SetLoops(-1, LoopType.Yoyo)
+                 .SetEase(Ease.InOutSine);
     }
 }

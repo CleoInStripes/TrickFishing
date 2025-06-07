f29899a51a65e24f3a4ed884ac0277607455ce52
Azee <aziztitu1996@gmail.com>
2025-06-07 01:42:37 -0700

    Adds HUD damage fx

--- Assets/Scripts/UI/PlayerHud.cs
+++ Assets/Scripts/UI/PlayerHud.cs
@@ -1,15 +1,23 @@
+using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 
-public class PlayerHud : MonoBehaviour
+public class PlayerHud : SingletonMonoBehaviour<PlayerHud>
 {
     public TextMeshProUGUI HealthHud;
     public TextMeshProUGUI ScoreHud;
 
+    [Header("Damage Screen")]
+    public MenuPage damageScreen;
+    public float damageScreenFlickerWaitTime = 3f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PlayerModel.Instance.health.OnDamageTaken.AddListener(() =>
+        {
+            FlickerDamageScreen();
+        });
     }
 
     // Update is called once per frame
@@ -21,11 +29,18 @@ public class PlayerHud : MonoBehaviour
 
     void UpdateHealth()
     {
-        HealthHud.text = $"Health: {PlayerModel.Instance.health.currentHealth}";
+        HealthHud.text = $"{PlayerModel.Instance.health.currentHealth}";
     }
 
     void UpdateScore()
     {
-        ScoreHud.text = $"Score: {PlayerModel.Instance.score}";
+        ScoreHud.text = $"{PlayerModel.Instance.score}";
+    }
+
+    async void FlickerDamageScreen()
+    {
+        damageScreen.Show();
+        await Task.Delay((int)(damageScreenFlickerWaitTime * 1000));
+        damageScreen.Hide();
     }
 }
